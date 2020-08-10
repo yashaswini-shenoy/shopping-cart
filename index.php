4f994daffa3a8999b24a926072389b8b041e4b38
yashaswini-shenoy <67504106+yashaswini-shenoy@users.noreply.github.com>
2020-08-10 07:39:24 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,81 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+   <link rel="stylesheet" type="text/css" href="styles.css">
+   <title>Login Page</title>
+</head>
+<body id="heya">
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<img id="image" src="images/logo.jpg" style=" height-300px;margin-right:10px; width:40px;border-radius:50%;">
+  <a class="navbar-brand" href="#">FashionCart</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div id="container">
+<div class="form-container">
+  <h2>Login</h2>
+  <form id="form" class="form-group" action="validation.php" method="post">
+  <label>Username</label>
+  <input type="text" name="username" class="form-control">
+  <label>Password</label>
+  <input type="password" name="password" class="form-control">
+  <button  id="button"  type="submit" name="submit" onclick="location.href='userinfo.php';" >Login</button>
+  
+  </form>
+  </div>
+  <div class="form-container">
+  <h2>Sign Up</h2>
+  <form id="form2" class="form-group" action="registration.php" method="post">
+  <label>Username</label>
+  <input type="text" name="username" class="form-control">
+  <label>Password</label>
+  <input type="password" name="password" class="form-control">
+  <button  id="button"  type="submit" name="submit" onclick="location.href='userinfo.php';" >SignUp</button>
+  
+  </form>
+  </div>
+</div>
+  </body>
+
+   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
